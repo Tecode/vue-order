95846e3a7e4c7fa42351fbaf48b18d6415befb43
谢明 <ming.xie@socialcredits.cn>
2018-05-21 12:16:38 +0800

    服务器发布地址

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 var express = require('express');
+var cookieParser = require('cookie-parser')
 var app = express();
+var axios = require('axios')
 
 app.use('/static', express.static('./dist/static'));
+app.use(cookieParser())
 app.get('*', function (req, res) {
-  res.sendfile('./dist/index.html');
+  if (!req.cookies.openId) {
+    res.redirect(`http://120.78.165.70/wechat/authorize?returnUrl=${'http://47.98.212.129:8080/#/?tableId=37'}`);
+  } else {
+    res.sendfile('./dist/index.html');
+  }
 });
+// export const authorizeApi = ({returnUrl}) => {
+//   return axios.post(`/wechat/authorize?returnUrl=${returnUrl}`)
+// }
 
 var server = app.listen(8080, function () {
   var host = server.address().address;
